10e046bb14d8fd38f33ea5a87e835d270e15f5eb
richardmaccaw <ricci6@gmail.com>
2018-11-22 12:03:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function getFirstSelector(selector) {
+    return document.querySelector(selector)
+}
+
+
+function nestedTarget() {
+    return document.querySelector('#nested .target')
+}
+
+
+const increaseRankBy = function (n) {
+    const list = document.querySelectorAll('.ranked-list li')
+    for (let i = 0; i < list.length; i++) {
+        let parsed = parseInt(list[i].innerHTML)
+        list[i].innerHTML = parsed + n
+    }
+}
+
+
+
+
+
+function deepestChild() {
+    const nodes = document.querySelectorAll('#grand-node div')
+    const endIndex = nodes.length -1
+    return nodes[endIndex]
+}
 
