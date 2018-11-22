dd183c201ab722bbe12239acf596e33a1ab5ba24
natashabuck <natashabuck1@gmail.com>
2018-11-22 11:57:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const getFirstSelector = (selector) => {
+    return document.querySelector(`${selector}`)
+};
 
+const nestedTarget = () => {
+    return document.querySelector('#nested .target')
+};
+
+const deepestChild = () => {
+   const grandNode = document.getElementById('grand-node').querySelectorAll('div')
+   return grandNode[grandNode.length - 1]
+};
+
+const increaseRankBy = (n) => {
+    const rankedLists = document.getElementsByClassName('ranked-list');
+
+    for (let i = 0; i<rankedLists.length; i++) {
+
+        let listEl = rankedLists[i].children;
+        
+        for(let x = 0; x<listEl.length; x++) {
+
+            listEl[x].innerHTML = parseInt(listEl[x].innerHTML) + n;
+        }
+    }
+    
+};
