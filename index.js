89d0d12c3cd4df5c2b885a22e2409c584ee80704
congo-quinine <congo.quinine@gmail.com>
2018-11-26 08:02:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const getFirstSelector = (selector) => {
+  return document.querySelector(selector)
+}
 
+const nestedTarget = () => {
+  return document.querySelector('#nested .target')
+}
+
+const deepestChild = () => document.querySelector('#grand-node div div div div');
+
+const increaseRankBy = (n) => {
+  const lis = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + 3
+  }
+}
